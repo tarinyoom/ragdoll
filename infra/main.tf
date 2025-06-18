afa995481475b9bc72aefc920ff7fbb3db3b7eb9
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2025-06-17 22:57:45 -0700

    Add cert

--- infra/main.tf
+++ infra/main.tf
@@ -58,6 +58,8 @@ resource "aws_s3_bucket_policy" "public_read" {
 }
 
 resource "aws_cloudfront_distribution" "cdn" {
+  depends_on = [aws_acm_certificate_validation.cert]
+
   origin {
     domain_name = aws_s3_bucket_website_configuration.site.website_endpoint
     origin_id   = "viteS3Origin"
@@ -125,4 +127,32 @@ output "bucket_name" {
 
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+data "aws_route53_zone" "this" {
+  name         = "tarinyoom.io."
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [
+    for record in aws_route53_record.cert_validation : record.value.fqdn
+  ]
 }
