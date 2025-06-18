996eeb1ec01496885f3d5c672ba0f06541fd17b9
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2025-06-17 23:47:30 -0700

    Remove route53

--- infra/main.tf
+++ infra/main.tf
@@ -128,31 +128,3 @@ output "bucket_name" {
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
-
-data "aws_route53_zone" "this" {
-  name         = "tarinyoom.io."
-  private_zone = false
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = data.aws_route53_zone.this.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 300
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [
-    for record in aws_route53_record.cert_validation : record.value.fqdn
-  ]
-}
